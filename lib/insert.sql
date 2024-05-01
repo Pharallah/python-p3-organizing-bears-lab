f836e1f97acb346fded15b320b9423f8e98de14f
Harry San Juan <harrycsanjuan@gmail.com>
2024-04-30 20:27:51 -0400

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "Brown", "Nice", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 23, "M", "Red", "Naughty", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 24, "F", "Purple", "Nice", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 25, "M", "Brown", "Nice", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 21, "F", "Yellow", "Naughty", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 26, "M", "Green", "Naughty", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 20, "F", "Red", "Naughty", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, "M", "Black", "Nice", "TRUE");
